996fd8f0e6e8264353375b053daa9e97d61930fc
TheScorpoi <sobral@ua.pt>
2020-11-11 18:00:18 +0000

    update

---
+++ procstat.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+########################################################################
+#
+#                            SO - Trabalho 1
+#                   Estatística de Processos em bash
+#
+# Este script permite a visualização da quantidade de memória total e da
+# memória residente em memória física, do número total de bytes I/O,e da
+# e taxa de leitura/escrita (bytes/sec) dos processos seleccionados nos
+# últimos s segundos.
+#
+# Pedro Sobral, nMec: 98491
+# Daniel Figueireo, nMec: 98498
+#
+# ######################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#template de como fazer um menu/aquilo dos -m -t -d -q -r ...
+#function menu() {
+#
+#    if [[ -v argOpt[n] ]]; then
+#        # ordenar por numero de sessoes
+#        printf "%s\n" "${userInfo[@]}" | sort -k2,2 ${order}
+#    else
+#        #ordem crescente (nome user)
+#        printf "%s\n" "${userInfo[@]}" | sort -k1,1 ${order}
+#    fi
+#}

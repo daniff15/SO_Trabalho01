2603a31eb099cc8a6d86a7840bf4fee6936bf53d
TheScorpoi <sobral@ua.pt>
2020-11-24 22:50:37 +0000

    fiz commit

--- procstat.sh
+++ procstat.sh
@@ -32,23 +32,32 @@
 
 
 # para fazer o raterw e o rater
-#ola=$(cat /proc/$$/io | grep rchar | tr -dc '0-9' )
-#echo $ola
-#sleep 3
-#adeus=$(cat /proc/$$/io | grep rchar | tr -dc '0-9') 
-#echo $adeus
+rchar1=$(cat /proc/$$/io | grep rchar | tr -dc '0-9' )
+wchar1=$(cat /proc/$$/io | grep wchar | tr -dc '0-9' ) 
+sleep 2
+rchar2=$(cat /proc/$$/io | grep rchar | tr -dc '0-9' ) 
+wchar2=$(cat /proc/$$/io | grep wchar | tr -dc '0-9' )
+rateR=$(echo "$rchar2/$rchar1" | bc -l)
+rateW=$(echo "$wchar2/$wchar1" | bc -l)
+echo "$rateR"
+echo "$rateW"
 
-for entry in /proc/*; do
-printf "%s %10s %10s %10s %10s\n" "VMSIZE" "VMRSS" "RCHAR" "WCHAR" "COMM" 
-    VmSize=$(cat $entry/status | grep VmSize | tr -dc '0-9')
-    VmRss=$(cat $entry/status | grep VmRSS | tr -dc '0-9')
-    rchar=$(cat $entry/io | grep rchar | tr -dc '0-9')
-    wchar=$(cat $entry/io | grep wchar | tr -dc '0-9' )
-    comm=$(cat $entry/comm)
-    printf "%d %10d %10d %10d %10s\n" $VmSize $VmRss $rchar $wchar $comm
 
-    echo "---------------------------------------------"
-done
+#printf "%s %10s %10s %10s %10s\n" "VMSIZE" "VMRSS" "RCHAR" "WCHAR" "COMM" 
+#for entry in /proc/*; do
+#    if [ -d $entry ]; then
+#        VmSize=$(cat $entry/status | grep VmSize | tr -dc '0-9')
+#        VmRss=$(cat $entry/status | grep VmRSS | tr -dc '0-9')
+#        if [ -x $entry ]; then
+#            rchar=$(cat $entry/io | grep rchar | tr -dc '0-9')
+#            wchar=$(cat $entry/io | grep wchar | tr -dc '0-9' )   
+#        fi
+#        comm=$(cat $entry/comm)
+#        printf "%d %10d %10d %10d %10s\n" $VmSize $VmRss $rchar $wchar $comm
+#        echo "--------------------------------------------------------------------"
+#    fi
+
+#done
     
 
 

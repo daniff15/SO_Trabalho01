e8b50dd0f30782bb96d2463a4842bfeb662473c7
TheScorpoi <sobral@ua.pt>
2020-11-27 16:42:19 +0000

    falta comm, print data, erros de pasta nao exist

--- procstat.sh
+++ procstat.sh
@@ -16,46 +16,44 @@
 
 
 
-# let's be intelegents and do some function, coz doing always comments its boring and wastefull
-
 function rates(){
-
-    ##
-    ## Isto esta mal, a conta é (rchar2 - rchar1) / sec
-    ##
+    
     rchar1=$(cat $entry/io | grep rchar | tr -dc '0-9')
     wchar1=$(cat $entry/io | grep wchar | tr -dc '0-9' )
-    sleep 1
-    rchar2=$(cat /proc/$entry/io | grep rchar | tr -dc '0-9' )
-    wchar2=$(cat /proc/$entry/io | grep wchar | tr -dc '0-9' )
-    rateR=$(echo "$rchar2/$1" | bc -l)
-    rateW=$(echo "$wchar2/$2" | bc -l)
+    sleep .1
+    rchar2=$(cat $entry/io | grep rchar | tr -dc '0-9' )
+    wchar2=$(cat $entry/io | grep wchar | tr -dc '0-9' )
+    rateR=$(echo "($rchar2-$rchar1)/0.1" | bc -l)
+    rateW=$(echo "($wchar2-$wchar1)/0.1" | bc -l)
 }
 
 
 function listarProcessos(){
     for entry in /proc/*; do
-        printf "%s %10s %10s %10s %10s %10s %10s %10s \n" "COMM" "USER" "VMSIZE" "VMRSS" "RCHAR" "WCHAR" "RATER" "RATEW"
-
-        PID=$(cat $entry/status | grep Pid)
-        echo $PID
-        #ps -o user= -p PID
+        printf "%s %10s %10s %10s %10s %10s %10s %10s %10s %15s\n" "COMM" "USER" "PID" "MEM" "RSS" "READB" "WRITEB" "RATER" "RATEW" "DATE"
+        
+        PID=$(cat $entry/status | grep -w Pid | tr -dc '0-9')
+        user=$(ps -o user= -p $PID)
+        startDate=$(ps -o lstart= -p $PID)
+        startDate=$(date +"%b %d %H:%M" -d "$startDate")
+        
         if [ -f $entry/status ]; then
             VmSize=$(cat $entry/status | grep VmSize | tr -dc '0-9')
             VmRss=$(cat $entry/status | grep VmRSS | tr -dc '0-9')
             if [ -x $entry ]; then
                 rchar1=$(cat $entry/io | grep rchar | tr -dc '0-9')
                 wchar1=$(cat $entry/io | grep wchar | tr -dc '0-9' )
-                #sleep .1
-                #rchar2=$(cat $entry/io | grep rchar | tr -dc '0-9' )
-                #wchar2=$(cat $entry/io | grep wchar | tr -dc '0-9' )
-                #rateR=$(echo "$rchar2/$rchar1" | bc -l)
-                #rateW=$(echo "$wchar2/$wchar1" | bc -l)
+                sleep 0.5
+                rchar2=$(cat $entry/io | grep rchar | tr -dc '0-9' )
+                wchar2=$(cat $entry/io | grep wchar | tr -dc '0-9' )
+                rateR=$(echo "($rchar2-$rchar1)/0.5" | bc -l)
+                rateW=$(echo "($wchar2-$wchar1)/0.5" | bc -l)
+                
             fi
         fi
         #comm=$(cat $entry/comm)
-        printf "%15s %10d %10d %10d %10d\n" $user $VmSize $VmRss $rchar1 $wchar1 #$rateR #$rateW #$comm
-        echo "=================================================================================="
+        printf "%15s %10d %10d %10d %10d %10d %10.1f %10.1f \n" $user $PID $VmSize $VmRss $rchar1 $wchar1 $rateR $rateW #$startDate #$comm
+        echo "============================================================================================================"
     done
 }
 listarProcessos

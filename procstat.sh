da0fbc90899509d2e7137cb72a5a1dd8b5c256ad
TheScorpoi <sobral@ua.pt>
2020-11-28 13:28:45 +0000

    configuracoes do menu e prints

--- procstat.sh
+++ procstat.sh
@@ -12,12 +12,14 @@
 # Pedro Sobral, nMec: 98491
 # Daniel Figueireo, nMec: 98498
 #
-# #######################################################################
+#########################################################################
 
 
-## Se estiver com pAtXORa, ir ver como por o header fixo
+#Arrays
+declare -A arrayAss=() # Array Associativo: está guardado a informação de cada processo, sendo a 'key' o PID
+declare -A argOpt=() # Array Associativo: está guardada a informação das opções passadas como argumentos na chamada da função
 
-## NAO SEI ATE QUE PONTO ISTO É UTIL/FUNCIONAL
+
 function opcoes(){
     echo "
     ************************************************************************************************
@@ -39,8 +41,56 @@ function opcoes(){
     ************************************************************************************************"
 }
 
-declare -A arrayAss=() # Array Associativo  
 
+#Tratamentos das opçoes passadas como argumentos
+
+while getopts "c:u:rs:e:dmtwp:" option; do
+    
+    case $option in
+        c) #Seleção de processos a utilizar atravez de uma expressão regular
+            #echo "-c was triggered! Parameter: $OPTARG"
+        ;;
+        s) #Seleção de processos a visualizar num periodo temporal - data mínima
+            #echo "-s was triggered! Parameter: $OPTARG"
+        ;;
+        e) #Seleção de processos a visualizar num periodo temporal - data máxima
+            #echo "-e was triggered! Parameter: $OPTARG"
+        ;;
+        u) #Seleção de processos a visualizar através do nome do utilizador
+            #echo "-u was triggered! Parameter: $OPTARG"
+        ;;
+        p) #Número de processos a visualizar
+            #echo "-p was triggered! Parameter: $OPTARG"
+        ;;
+        m) #Ordenação da tabela por MEM (decrescente)
+            
+        ;;
+        t) #Ordenação da tabela por RSS (decrescente)
+            
+        ;;
+        d) #Ordenação da tabela por RATER (decrescente)
+            
+        ;;
+        w) #Ordenação da tabela pOR RATEW (decrescente)
+            
+        ;;
+        r) #Ordenação reversa
+            
+        ;;
+        *) #Passagem de argumentos inválidos
+            opcoes
+            exit
+        ;;
+    esac
+    
+    if [[ -x "$OPTARG" ]]; then
+        argOpt[$option]=${OPTARG}
+    else
+        argOpt[$option]=$[OPTARG]
+    fi
+done
+
+#Tratamento dos dados lidos
 function listarProcessos(){
     #Cabeçalho
     printf "%-30s %-16s %15s %12s %12s %12s %12s %12s %12s %16s\n" "COMM" "USER" "PID" "MEM" "RSS" "READB" "WRITEB" "RATER" "RATEW" "DATE"
@@ -51,76 +101,68 @@ function listarProcessos(){
             startDate=$(ps -o lstart= -p $PID)                                  #data de início do processo atraves do PID
             startDate=$(date +"%b %d %H:%M" -d "$startDate")                    #formatação da data conforme o guião
             
-            VmSize=$(cat $entry/status | grep VmSize | tr -dc '0-9')        
+            VmSize=$(cat $entry/status | grep VmSize | tr -dc '0-9')
             VmRss=$(cat $entry/status | grep VmRSS | tr -dc '0-9')
             
             if [[ VmSize -ne 0 || VmRss -ne 0 ]]; then
                 if [ -f $entry/status ]; then
                     
-                    rchar1=$(cat $entry/io | grep rchar | tr -dc '0-9')         #rchar inicial
-                    wchar1=$(cat $entry/io | grep wchar | tr -dc '0-9' )        #wchar inicial
-                    sleep $1                                                    #tempo em espera
-                    rchar2=$(cat $entry/io | grep rchar | tr -dc '0-9' )        #rchar apos s segundos
-                    wchar2=$(cat $entry/io | grep wchar | tr -dc '0-9' )        #wchar apos s segundos
-                    rateR=$(echo "($rchar2-$rchar1)/$1" | bc )                  #calculo do rateR
-                    rateW=$(echo "($wchar2-$wchar1)/$1" | bc )                  #calculo do rateW
-                    
+                    #rchar1=$(cat $entry/io | grep rchar | tr -dc '0-9')         #rchar inicial
+                    #wchar1=$(cat $entry/io | grep wchar | tr -dc '0-9' )        #wchar inicial
+                    #sleep $1                                                    #tempo em espera
+                    #rchar2=$(cat $entry/io | grep rchar | tr -dc '0-9' )        #rchar apos s segundos
+                    #wchar2=$(cat $entry/io | grep wchar | tr -dc '0-9' )        #wchar apos s segundos
+                    #rateR=$(echo "($rchar2-$rchar1)/$1" | bc )                  #calculo do rateR
+                    #rateW=$(echo "($wchar2-$wchar1)/$1" | bc )                  #calculo do rateW
+                    rateR=0
+                    rateW=0
+
                 fi
                 comm=$(cat $entry/comm)
-
+                
                 #fizemos arrayAss[$PID] pelo $PID, pq cada processo tem um diferente, e é uma boa maniera de os distinguir visto que assim não há colisão de informação
                 arrayAss[$PID]=$(printf "%-30s %-16s %15d %12d %12d %12d %12d %12.1f %12.1f %16s\n" "$comm" "$user" "$PID" "$VmSize" "$VmRss" "$rchar1" "$wchar1" "$rateR" "$rateW"  "$startDate");
-
+                
             fi
         fi
     done
     
-    printf '%s \n' "${arrayAss[@]}" | sort -n -k1                               #-n sort crescente -rn sort decrescente -k'?' para ordenar por essa ordem (-rn ou -n) a coluna '?'
-    
+    prints
 }
 
-if [[ $# == 1 ]]; then
-        listarProcessos $1
-    fi
 
-while getopts "c:u:rs:e:dmtwp:" opt; do
+function prints(){
     
-    case $opt in
-        c)
-            echo "-c was triggered! Parameter: $OPTARG"
-        ;;
-        s)
-            echo "-s was triggered! Parameter: $OPTARG"
-        ;;
-        e)
-            echo "-e was triggered! Parameter: $OPTARG"
-        ;;
-        u)
-            echo "-u was triggered! Parameter: $OPTARG"
-        ;;
-        p)
-            echo "-p was triggered! Parameter: $OPTARG"
-        ;;
-        m)
-            echo "-b was triggered!"
-        ;;
-        t)
-            echo "-b was triggered!"
-        ;;
-        d)
-            echo "-b was triggered!"
-        ;;
-        w)
-            echo "-b was triggered!"
-        ;;
-        r)
-            echo "-b was triggered!"
-        ;;
-        *)
-            opcoes
-        ;;
-    esac
-done
+    if [[ -v argOpt[r] ]]; then
+        ordem = "-n"
+    else
+        ordem = "-rn"
+    fi
+    
+    if [[ -v argOpt[m] ]]; then
+        #Ordenação da tabela pelo MEM
+        printf '%s \n' "${arrayAss[@]}" | sort $order -k4,4
+        
+    elif [[ -v argOpt[t] ]]; then
+        #Ordenação da tabela prlo RSS
+        printf '%s \n' "${arrayAss[@]}" | sort $order -k5
+        
+    elif [[ -v argOpt[d] ]]; then
+        #Ordenação da tabela pelo RATER
+        printf '%s \n' "${arrayAss[@]}" | sort $order -k8
+        
+    elif [[ -v argOpt[w] ]]; then
+        #Ordenação da tabela pelo RATEW
+        printf '%s \n' "${arrayAss[@]}" | sort $order -k9
+        
+    else [[ -v argOpt[n] ]]
+        #Ordenação default da tabela, ordem alfabética dos processos
+        printf '%s \n' "${arrayAss[@]}" | sort $order -k1
+        
+    fi 
+}
+
+listarProcessos
 
 #template de como fazer um menu/aquilo dos -m -t -d -q -r ...
 #function menu() {

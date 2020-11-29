2eb6dfe159fbb041082c990154cfe7e0669a9d42
TheScorpoi <sobral@ua.pt>
2020-11-29 23:46:05 +0000

    final

--- procstat.sh
+++ procstat.sh
@@ -13,12 +13,13 @@
 #########################################################################################################################
 
 
-
 #Arrays
 declare -A arrayAss=() # Array Associativo: está guardado a informação de cada processo, sendo a 'key' o PID
-declare -A argOpt=()   # Array Associativo: está guardada a informação das opções passadas como argumentos na chamada da função #fizemos arrayAss[$PID] pelo $PID, pq cada processo tem um diferente, e é uma boa maniera de os distinguir visto que assim não há colisão de informação
-i=0                    #iniciação da variável i, usada na condição do argOpt[p]
+declare -A argOpt=()   # Array Associativo: está guardada a informação das opções passadas como argumentos na chamada da função
 
+i=0                    #iniciação da variável i, usada na condição do argOpt[p] da função listarProcessos()
+
+#Função para quando argumentos passados são inválidos
 function opcoes() {
     echo "************************************************************************************************"
     echo "OPÇÃO INVÁLIDA!"
@@ -36,25 +37,23 @@ function opcoes() {
 }
 
 #Tratamentos das opçoes passadas como argumentos
-
 while getopts "c:u:rs:e:dmtwp:" option; do
 
     case $option in
     c) #Seleção de processos a utilizar atraves de uma expressão regular
-        #echo "-c was triggered! Parameter: $OPTARG"
 
         ;;
     s) #Seleção de processos a visualizar num periodo temporal - data mínima
-        #echo "-s was triggered! Parameter: $OPTARG"
+
         ;;
     e) #Seleção de processos a visualizar num periodo temporal - data máxima
-        #echo "-e was triggered! Parameter: $OPTARG"
+
         ;;
     u) #Seleção de processos a visualizar através do nome do utilizador
-        #echo "-u was triggered! Parameter: $OPTARG"
+
         ;;
     p) #Número de processos a visualizar
-        #echo "-p was triggered! Parameter: $OPTARG"
+
         ;;
     m) #Ordenação da tabela por MEM (decrescente)
         ;;
@@ -76,16 +75,17 @@ while getopts "c:u:rs:e:dmtwp:" option; do
         ;;
     esac
 
+    #Adicionar ao array argOpt as opcoes passadas ao correr o procstat.sh, caso existam adiciona as que são passadas, caso não, adiciona "nada"
     if [[ -z "$OPTARG" ]]; then
-        argOpt[$option]="none"
+        argOpt[$option]="nada"
     else
         argOpt[$option]=${OPTARG}
     fi
 done
 
-
 #Tratamento dos dados lidos
 function listarProcessos() {
+    
     #Cabeçalho
     printf "%-30s %-16s %15s %12s %12s %12s %12s %12s %12s %16s\n" "COMM" "USER" "PID" "MEM" "RSS" "READB" "WRITEB" "RATER" "RATEW" "DATE"
     for entry in /proc/*; do
@@ -95,13 +95,12 @@ function listarProcessos() {
             startDate=$(ps -o lstart= -p $PID)                              # data de início do processo atraves do PID
             startDate=$(date +"%b %d %H:%M" -d "$startDate")                # formatação da data conforme o guião
 
-            VmSize=$(cat $entry/status | grep VmSize | tr -dc '0-9')
-            VmRss=$(cat $entry/status | grep VmRSS | tr -dc '0-9')
+            VmSize=$(cat $entry/status | grep VmSize | tr -dc '0-9')        # ir buscar o VmSize
+            VmRss=$(cat $entry/status | grep VmRSS | tr -dc '0-9')          # ir buscar o VmRss
 
-            #nao sei se este if faz sentido
-            if [[ VmSize -ne 0 || VmRss -ne 0 ]]; then
+            if [[ VmSize -ne 0 || VmRss -ne 0 ]]; then                       
                 if [ -f $entry/status ]; then
-                    sec=$1
+                    sec=$1                                                  # guardar em sec os segundos passados nos argumentos
                     rchar1=$(cat $entry/io | grep rchar | tr -dc '0-9')     # rchar inicial
                     wchar1=$(cat $entry/io | grep wchar | tr -dc '0-9')     # wchar inicial
                     sleep $sec                                              # tempo em espera
@@ -111,7 +110,7 @@ function listarProcessos() {
                     rateW=$(echo "($wchar2-$wchar1)/$sec" | bc)             # calculo do rateW
 
                 fi
-                comm=$(cat $entry/comm)
+                comm=$(cat $entry/comm)                                     # ir buscar o comm
 
                 #Seleção de processos a visualizar num periodo temporal
                 if [[ -v argOpt[s] && -v argOpt[e] ]]; then
@@ -138,7 +137,8 @@ function listarProcessos() {
                     if [[ $comm =~ ${argOpt['c']} ]]; then
                         arrayAss[$PID]=$(printf "%-30s %-16s %15d %12d %12d %12d %12d %12.1f %12.1f %16s\n" "$comm" "$user" "$PID" "$VmSize" "$VmRss" "$rchar1" "$wchar1" "$rateR" "$rateW" "$startDate")                        
                     fi
-
+                
+                #Seleção de processos a utilizar com expressão regular e número de processos a utilizar
                 elif [[ -v argOpt[c] && -v argOpt[p] ]]; then
                     
                     if [[ $comm =~ ${argOpt['c']} && $i < ${argOpt['p']} ]]; then
@@ -158,11 +158,8 @@ function listarProcessos() {
                 else
                     arrayAss[$PID]=$(printf "%-30s %-16s %15d %12d %12d %12d %12d %12.1f %12.1f %16s\n" "$comm" "$user" "$PID" "$VmSize" "$VmRss" "$rchar1" "$wchar1" "$rateR" "$rateW"  "$startDate");
                 fi
-
-
             fi
         fi
-
     done
 
     prints
@@ -181,7 +178,7 @@ function prints() {
         printf '%s \n' "${arrayAss[@]}" | sort $ordem -k4
 
     elif [[ -v argOpt[t] ]]; then
-        #Ordenação da tabela prlo RSS
+        #Ordenação da tabela pelo RSS
         printf '%s \n' "${arrayAss[@]}" | sort $ordem -k5
 
     elif [[ -v argOpt[d] ]]; then
@@ -202,4 +199,4 @@ function prints() {
 
 }
 
-listarProcessos ${@: -1}            #este agumento passado é para os segundos, visto que e passado em todas as opções
+listarProcessos ${@: -1}            #este agumento passado é para os segundos, visto que é passado em todas as opções, e é sempre o último
